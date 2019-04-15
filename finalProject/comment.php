3f9483b199f4f91c2a78b9f9050817784dbbc01d
Ricardo Quiroga <ricardo.j.quiroga@wmich.edu>
2019-04-15 18:28:48 -0400

    Fixed a problem with comments not being posted.
    For some reason the database wouldn't fill in a UserID when a new 
    comment was posted. I had to make a function to get it to work right.

--- finalProject/comment.php
+++ finalProject/comment.php
@@ -1,20 +1,28 @@
-<?php 
+<?php
 define('DBHOST', 'localhost');
-define('DBNAME', 'bread');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING', 'mysql:dbname=bread;charset=utf8mb4;');
+define('DBNAME', 'id9307686_bread');
+define('DBUSER', 'id9307686_breaduser');
+define('DBPASS', '12345');
+define('DBCONNSTRING', 'mysql:host=localhost;dbname=id9307686_bread;charset=utf8mb4;');
+
+function getMax($connection) {
+	$rowSQL = mysqli_query($connection, "SELECT MAX(UserID) AS max FROM `comments`");
+	$row = mysqli_fetch_array($rowSQL);
+	return ++$row['max'];
+}
+
 $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
 
+$largestNumber = getMax($connection);
 $name = $_POST['name'];
 $comment = $_POST['comment'];
-$sql = "INSERT INTO comments (UserName, Comment) VALUES ('$name', '$comment')";
+$sql = "INSERT INTO comments (UserID, UserName, Comment) VALUES ('$largestNumber', '$name', '$comment')";
 
 if($result = mysqli_query($connection, $sql)){
 	header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 else {
 	echo "We're sorry, something went wrong and your comment was not posted";
-	
+
 }
 ?>
